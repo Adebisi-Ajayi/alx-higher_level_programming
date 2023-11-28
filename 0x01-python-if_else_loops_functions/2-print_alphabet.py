2df3ec89b2df40ffa7034a60d85a32f1bf3c5559
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-11-28 05:16:57 -0800

    task2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,11 +1,4 @@
-# Print the ASCII alphabet in lowercase without a new line
-
-# Loop through the ASCII values for lowercase letters (97 to 122)
+#!/usr/bin/python3
 for i in range(97, 123):
-    # Use the chr() function to convert the ASCII value to a character
-    # Use the end parameter of the print function to avoid a new line
     print(chr(i), end='')
-
-# Print a new line after printing all the characters
-print()
-
+    print()
