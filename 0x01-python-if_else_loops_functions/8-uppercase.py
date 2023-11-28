9c0805961f7b9cb597835a49293d553fb8558a8c
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-11-28 09:26:12 -0800

    Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(input_str):
+    for char in input_str:
+        uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
+        print("{}".format(uppercase_char), end='')
+
+    # Print a new line after the string
+    print()
