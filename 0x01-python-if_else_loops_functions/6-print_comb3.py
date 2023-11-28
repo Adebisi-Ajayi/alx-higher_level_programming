f9dffd0f0915f5df562682048763a5de7f799d29
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-11-28 09:08:38 -0800

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,4 @@
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
         print("{:02}, ".format(tens_digit * 10 + ones_digit), end="")
-
-# Print a new line after the last combination
-print()
-
+print ()
