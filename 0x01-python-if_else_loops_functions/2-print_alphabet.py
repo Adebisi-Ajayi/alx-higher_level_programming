4881d9f36d4ae12008ea6c69b0f95786329b4127
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-11-28 08:49:15 -0800

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-# Print ASCII alphabet in lowercase without a new line
-print(''.join(chr(i) for i in range(ord('a'), ord('z') + 1)), end='')
+for char in range(ord('a'), ord('z')+1):
+    print("{}".format(chr(char)), end='')
+
+# Output: abcdefghijklmnopqrstuvwxyz
 
