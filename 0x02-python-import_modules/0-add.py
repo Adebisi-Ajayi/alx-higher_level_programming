6714f2d2ad749456b6e574c0c119f934413ff390
Adebisi-Ajayi <ajayielkanah55@gmail.com>
2023-12-02 03:56:37 -0800

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,6 @@
-# Save this code in a file, let's say main.py
-
+#!/usr/bin/python3
+from 0-add import add
 a = 1
 b = 2
-
-# Import the add function from add_0.py
-from add_0 import add
-
 # Print the result using string format
 print("{} + {} = {}".format(a, b, add(a, b)))
